dbd3919be80a0216be164e7b1916571f867c0032
jlibermann <git@github.com>
2020-08-24 11:10:20 -0400

    Small cloudbuild fix

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -10,6 +10,6 @@ echo "Building gcr.io/panoptes-exp/panoptes-tom:${TAG}"
 gcloud builds submit \
     --timeout="5h" \
     --substitutions "_TAG=${TAG}" \
-    --config "${SOURCE_DIR}/Docker/${BASE_CLOUD_FILE}" \
+    --config "${SOURCE_DIR}/${BASE_CLOUD_FILE}" \
     "${SOURCE_DIR}" \
     --machine-type=n1-highcpu-8
